6e78fd9619ee685071619066ddc40345d8173522
shubham <shubhamv430@gmail.com>
2021-10-03 09:59:30 +0530

    resolved error

--- src/server.js
+++ src/server.js
@@ -3,10 +3,6 @@ require("dotenv").config();
 const app = express();
 const cors = require("cors");
 const connect = require("./config/db");
-<<<<<<< HEAD
-=======
-const cors = require("cors");
->>>>>>> ravi
 app.use(cors());
 app.use(express.json());
 const Msg = require("./models/msg.model");
